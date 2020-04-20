8430d158172fa5bb9a10fc03bafb286179988d47
Leonardo Teixeira <leovenancio00@gmail.com>
2020-04-20 14:41:43 -0300

    Link service trocado para o do umbler

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map'; //biblioteca via comando
 })
 export class ApiServiceService {
 
-  server: string = "http://localhost/apiAngular/";
+  server: string = "http://cinca-com-br.umbler.net/";
   constructor(private http: HttpClient) { }
 
   Api(dados: any, api: string) {
