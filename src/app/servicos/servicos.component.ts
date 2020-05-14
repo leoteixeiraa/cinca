9ee65c31d55109ecb947eda432523d5b1ecb6b37
Leonardo Teixeira <leovenancio00@gmail.com>
2020-05-14 17:28:01 -0300

    Modulo de servicos

--- src/app/servicos/servicos.component.ts
+++ src/app/servicos/servicos.component.ts
@@ -14,19 +14,144 @@ export class ServicosComponent implements OnInit {
   limit = 1000;
   start = 0;
 
+
   idServico = '';
   cod_lcin = '';
-  unidade = '';
   descricao = '';
+  custoUnit = '';
+  marca = '';
+  observacoes = '';
   title = 'Inserir Serviço';
   textoBuscar = '';
   caminho = 'apiServicos.php';
   ApiServiceService;
 
+  totalRecords: String;
+  paginaAtual: number = 1;
+
   constructor(private provider: ApiServiceService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.lista = [];
+    this.start = 0;
+    this.carregar(this.textoBuscar);
 
+
+  }
+
+
+  carregar(texto: string) {
+    this.lista = [];
+    this.start = 0;
+    return new Promise(resolve => {
+      const dados = {
+        requisicao: 'listar',
+        limit: this.limit,
+        start: this.start,
+        textoBuscar: texto
+      };
+      this.provider.Api(dados, this.caminho).subscribe(data => {
+        for (const dado of data['result']) {
+          this.lista.push(dado);
+
+
+        }
+        resolve(true);
+
+      });
+
+    });
+  }
+
+  cadastrar() {
+    // tslint:disable-next-line: max-line-length
+    if (this.cod_lcin !== '' && this.descricao !== '' && this.custoUnit !== '') {
+      return new Promise(resolve => {
+        const dados = {
+          requisicao: 'add',
+          cod_lcin: this.cod_lcin,
+          descricao: this.descricao,
+          custoUnit: this.custoUnit,
+          marca: this.marca,
+          observacoes: this.observacoes,
+        };
+        this.provider.Api(dados, this.caminho)
+          .subscribe(data => {
+
+            if (data['success']) {
+              alert('Salvo com sucesso!!');
+
+            } else {
+              alert('Erro ao Salvar!!');
+            }
+
+          });
+      });
+    } else {
+      alert('Prencha os Campos!');
+    }
+  }
+
+  // tslint:disable-next-line: max-line-length
+  dadosEditar(cod_lcin: string, descricao: string, custoUnit: string, marca: string, observacoes: string, idServico: string) {
+    this.title = 'Editar Servico';
+    this.cod_lcin = cod_lcin;
+    this.descricao = descricao;
+    this.custoUnit = custoUnit;
+    this.marca = marca;
+    this.observacoes = observacoes;
+    this.idServico = idServico;
+  }
+
+  editar() {
+    return new Promise(resolve => {
+      const dados = {
+        requisicao: 'editar',
+        cod_lcin: this.cod_lcin,
+        descricao: this.descricao,
+        custoUnit: this.custoUnit,
+        marca: this.marca,
+        observacoes: this.observacoes,
+        idServico: this.idServico
+      };
+      this.provider.Api(dados, this.caminho)
+        .subscribe(data => {
+
+          if (data['success']) {
+            alert('Editado com sucesso!!');
+
+            //  location='linhas';
+            this.router.navigateByUrl('/materiais.html');
+
+
+          } else {
+            alert('Erro ao Editar!!');
+          }
+
+        });
+    });
+
+  }
+
+  excluir(idu: string) {
+    return new Promise(resolve => {
+      const dados = {
+        requisicao: 'excluir',
+        idServico: idu
+      };
+      this.provider.Api(dados, this.caminho)
+        .subscribe(data => {
+
+          if (data['success']) {
+            alert('Excluido com sucesso!');
+            this.router.navigateByUrl('/materiais.html');
+
+          } else {
+            alert('Erro ao Excluir!!');
+          }
+
+        });
+    });
   }
 
 }
